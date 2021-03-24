0bd38034f24b14b7b8a1efded51a99e4dcc2d56f
Felix Pronove <felixpronove4@gmail.com>
2021-03-23 23:30:19 -0600

    Added javascript to add a row to the table in my website for each UFO sighting

--- UFO-level-1/index.js
+++ UFO-level-1/index.js
@@ -1,3 +1,21 @@
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Console.log the weather data from data.js
+console.log(data);
+
+// Step 1: Loop Through `data`, add a row to the table for each UFO sighting
+data.array.forEach(function(UFOsighting) => {
+    console.log(UFOsighting);
+    var row=tbody.append('tr');
+
+    Object.entries(UFOsighting).forEach(function([key, value]) {
+        console.log(key, value);
+    var cell = row.append("td");
+    cell.text(value);
+    });
+});
+
 // write code that appends a table to your web page and then adds new rows of data for each UFO sighting.
 
-// create an empty array to store all information about UFO sightings
+// Make sure you have a column for `date/time`, `city`, `state`, `country`, `shape`, and `comment` at the very least.
