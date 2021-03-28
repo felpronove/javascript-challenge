1cf8198eb341e270651c4d4eef7aaae325756a59
Felix Pronove <felixpronove4@gmail.com>
2021-03-27 21:32:51 -0600

    Adding more search filters to html and index files

--- UFO-level-2/index.js
+++ UFO-level-2/index.js
@@ -1,3 +1,5 @@
+// PHASE 1
+
 // write code that appends a table to your web page and then adds new rows of data for each UFO sighting.
 // Make sure you have a column for `date/time`, `city`, `state`, `country`, `shape`, and `comment` at the very least.
 
@@ -57,9 +59,77 @@ function DateFilter(){
 d3.select("#filter-btn").on("click", DateFilter);
 d3.select("#form").on("submit", DateFilter);
 
+// PHASE 2
+// Using multiple `input` tags and/or select dropdowns, write JavaScript code so the user can to set multiple filters and search for UFO sightings
+
+// create a function that will create a filter for city 
+function CityFilter(){
+    // pull data for UFO sightings 
+     var filterCity = UFOsighting
+     d3.event.preventDefault();
+
+     // Set up a variable to hold the city entered by user
+     var userCity=d3.select("#city").property("value");
+
+     // Use input from field to filter the data for city
+     // if user enters a city, filter data to include sightings that match the criteria
+     if(userCity) {
+         filterCity=filterCity.filter(item => item.city===userCity);
+     };
+
+     // run the createTable function with the filtered data
+     createTable(filterCity);
+}
+
 // Create event handlers for clicking the button or pressing the enter key
-// button.on("click", DateFilter);
-// form.on("submit", DateFilter);
+d3.select("#city-filter-btn").on("click", cityFilter);
+d3.select("#form").on("submit", cityFilter);
+
+// create a function that will create a filter for state 
+function StateFilter(){
+    // pull data for UFO sightings 
+     var filterState = UFOsighting
+     d3.event.preventDefault();
+
+     // Set up a variable to hold the state entered by user
+     var userState=d3.select("#state").property("value");
+
+     // Use input from field to filter the data for state
+     // if user enters a state, filter data to include sightings that match the criteria
+     if(userState) {
+         filterState=filterState.filter(item => item.state===userState);
+     };
+
+     // run the createTable function with the filtered data
+     createTable(filterState);
+}
+
+// Create event handlers for clicking the button or pressing the enter key
+d3.select("#state-filter-btn").on("click", StateFilter);
+d3.select("#form").on("submit", StateFilter);
+
+// create a function that will create a filter for country 
+function CountryFilter(){
+    // pull data for UFO sightings 
+     var filterCountry = UFOsighting
+     d3.event.preventDefault();
+
+     // Set up a variable to hold the country entered by user
+     var userCountry=d3.select("#country").property("value");
+
+     // Use input from field to filter the data for country
+     // if user enters a country, filter data to include sightings that match the criteria
+     if(userCountry) {
+         filterCountry=filterCountry.filter(item => item.country===userCountry);
+     };
+
+     // run the createTable function with the filtered data
+     createTable(filterCountry);
+}
+
+// Create event handlers for clicking the button or pressing the enter key
+d3.select("#country-filter-btn").on("click", CountryFilter);
+d3.select("#form").on("submit", CountryFilter);
 
 // call build table function no matter if filter function goes or not
 
