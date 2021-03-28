232267d8cc7400357cde87395088a59c173c4b13
Felix Pronove <felixpronove4@gmail.com>
2021-03-27 20:30:19 -0600

    copied all files for phase 2

---
+++ UFO-level-2/index.js
@@ -0,0 +1,67 @@
+// write code that appends a table to your web page and then adds new rows of data for each UFO sighting.
+// Make sure you have a column for `date/time`, `city`, `state`, `country`, `shape`, and `comment` at the very least.
+
+// Assign the data from `data.js` to a descriptive variable
+var UFOsighting = data;
+
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// build a table to show all data results
+// table needs to clear previousl results with each search
+// Use D3 to select the table
+var table = d3.select("#ufo-table");
+
+// Use D3 to set the table class to `table table-striped`
+table.attr("class", "table table-striped");
+
+// Create a function to create a table with all returned UFO entries
+function createTable(UFOdata){
+    // clear table of all previous searches
+    tbody.html("")
+    // Loop through each data entry to create a table row for each entry
+    UFOdata.forEach((sighting) => {
+        console.log(sighting);
+        var row=tbody.append('tr');
+    // Add table columns for each key value pair in each entry
+        Object.values(sighting).forEach((value) =>{
+            console.log(value);
+        var cell = row.append("td");
+        cell.text(value);
+        });
+    });
+}
+// Use a date form in your HTML document and write JavaScript code that will listen for events
+// Search through the `date/time` column to find rows that match user input.
+
+// create a function that will create a filter for datetime 
+function DateFilter(){
+    // pull data for UFO sightings 
+     var filterDateTime = UFOsighting
+     d3.event.preventDefault();
+
+     // Set up a variable to hold the datetime entered by user
+     var userDateTime=d3.select("#datetime").property("value");
+
+     // Use input from field to filter the data for datetime
+     // if user enters a datetime, filter data to include sightings that match the criteria
+     if(userDateTime) {
+         filterDateTime=filterDateTime.filter(item => item.datetime===userDateTime);
+     };
+
+     // run the createTable function with the filtered data
+     createTable(filterDateTime);
+}
+
+// Create event handlers for clicking the button or pressing the enter key
+d3.select("#filter-btn").on("click", DateFilter);
+d3.select("#form").on("submit", DateFilter);
+
+// Create event handlers for clicking the button or pressing the enter key
+// button.on("click", DateFilter);
+// form.on("submit", DateFilter);
+
+// call build table function no matter if filter function goes or not
+
+// Run createTable function for entire dataset if no filter has been selected
+createTable(UFOsighting);
