5accbbc0c5c19446475a5495c544544cc980ebd2
Felix Pronove <felixpronove4@gmail.com>
2021-03-27 20:21:13 -0600

    updated the logic for the datetime filter

--- UFO-level-1/index.js
+++ UFO-level-1/index.js
@@ -10,19 +10,21 @@ var tbody = d3.select("tbody");
 // build a table to show all data results
 // table needs to clear previousl results with each search
 // Use D3 to select the table
-var table = d3.select("table");
+var table = d3.select("#ufo-table");
 
 // Use D3 to set the table class to `table table-striped`
 table.attr("class", "table table-striped");
 
 // Create a function to create a table with all returned UFO entries
 function createTable(UFOdata){
+    // clear table of all previous searches
+    tbody.html("")
     // Loop through each data entry to create a table row for each entry
     UFOdata.forEach((sighting) => {
         console.log(sighting);
         var row=tbody.append('tr');
     // Add table columns for each key value pair in each entry
-        Object.values(sighting).forEach((value) {
+        Object.values(sighting).forEach((value) =>{
             console.log(value);
         var cell = row.append("td");
         cell.text(value);
@@ -33,14 +35,15 @@ function createTable(UFOdata){
 // Search through the `date/time` column to find rows that match user input.
 
 // create a function that will create a filter for datetime 
-function DateFilter(datetime){
-     // Use input from field to filter the data for datetime
-     var filterDateTime = alienReporting
+function DateFilter(){
+    // pull data for UFO sightings 
+     var filterDateTime = UFOsighting
      d3.event.preventDefault();
 
      // Set up a variable to hold the datetime entered by user
-     var userDateTime=d3.select("datetime").property("value");
+     var userDateTime=d3.select("#datetime").property("value");
 
+     // Use input from field to filter the data for datetime
      // if user enters a datetime, filter data to include sightings that match the criteria
      if(userDateTime) {
          filterDateTime=filterDateTime.filter(item => item.datetime===userDateTime);
@@ -50,15 +53,13 @@ function DateFilter(datetime){
      createTable(filterDateTime);
 }
 
-// Select the button
-var button = d3.select("#button");
-
-// Select the form
-var form = d3.select("#form");
+// Create event handlers for clicking the button or pressing the enter key
+d3.select("#filter-btn").on("click", DateFilter);
+d3.select("#form").on("submit", DateFilter);
 
 // Create event handlers for clicking the button or pressing the enter key
-button.on("click", //filterfunction);
-form.on("submit", //filterfunction);
+// button.on("click", DateFilter);
+// form.on("submit", DateFilter);
 
 // call build table function no matter if filter function goes or not
 
